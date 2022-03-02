a809d1d74770064a6c276aacd2693407599b607d
dhana <dhanak983@gmail.com>
2022-03-02 15:09:49 -0600

    changes

---
+++ Assignments/index.js
@@ -0,0 +1,18 @@
+/* let age=565;
+console.log(typeof age);
+let a =5, b = 7;
+if(a<b){
+    var c = 50;
+}
+console.log(c); */
+//backtik expression
+/* let a= 10, b = 20;
+console.log(`sum of a and b: ${a+b}`); //expression is always followed by $ */
+let age= 16;
+if(age>18){
+    console.log('eligible to vote');
+
+}
+else{
+    console.log('not eligible to vote')
+}
