b55d0f8e4f0a97d6eca4652820846d4d7cac50ab
dhana <dhanak983@gmail.com>
2022-03-09 20:28:32 -0600

    changes

---
+++ colors.js
@@ -0,0 +1,17 @@
+// function colours(){
+// return  ["white", "blue", "yellow", "black" , "red ", "green"]
+// }
+// let [firstcolour,secondcolour,...othercolours] = colours();
+// console.log(firstcolour);
+// console.log(secondcolour);
+// console.log(othercolours);
+
+const myClasses = {
+    firstHour: "Ethics",
+    secondHour: "Programming",
+    thirdHour: "Biology"
+
+}
+
+displayClasses(secondHour) => {firsthour,secondHour,ThirdHour} 
+console.log("timefor pogramming: "+ ""+ secondHour +"class");
