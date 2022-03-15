09d8d7d78fb3fe3c75e84b1c7e0d67d0b2496d69
dhana <dhanak983@gmail.com>
2022-03-15 16:26:22 -0500

    Quiz Assignment

---
+++ Project/quizquestions.js
@@ -0,0 +1,68 @@
+let questions = [
+    {
+    numb: 1,
+    question: "The CSS property used to control the element's font-size is?",
+    answer: "font-size",
+    options: [
+        "text-style",
+        "text-size",
+        "font-size",
+        " None of the above"
+    ]
+  },
+    {
+    numb: 2,
+    question: "The property in CSS used to change the background color of an element is?",
+    answer: "background-color",
+    options: [
+        "bgcolor",
+        "color",
+        "background-color",
+        "All of the above"
+    ]
+  },
+    {
+    numb: 3,
+    question: "The HTML attribute used to define the inline styles is?",
+    answer: "style",
+    options: [
+      "style",
+      "styles",
+      "class",
+      "None of the above"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Which of the following property is used as the shorthand property for the padding properties?",
+    answer: "padding",
+    options: [
+      "padding-left",
+      "padding-right",
+      "padding",
+      "All of the above"
+    ]
+  },
+  {
+   numb: 6,
+    question: "Are the negative values allowed in padding property?",
+    answer: "no",
+    options: [
+      "Yes",
+      " No",
+      "Can't say",
+      "May be"
+    ]
+  }
+];
